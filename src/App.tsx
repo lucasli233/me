c544b7057272270b056236ae72885d3ba73e0c2f
Oblivion233 <lucaaas2333@outlook.com>
2020-10-01 22:38:41 +1300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      {/* <Header /> */}
+      <div className="container">
+        <h1>About Me</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          egestas elit a tellus porttitor, id finibus ligula pulvinar. Phasellus
+          nec posuere nisl. Vivamus ac turpis a velit vestibulum lacinia eget ac
+          dui. Nunc efficitur libero tortor, quis aliquam nunc tristique eu. In
+          egestas lectus a porta imperdie
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h1>My Interests</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          egestas elit a tellus porttitor, id finibus ligula pulvinar. Phasellus
+          nec posuere nisl. Vivamus ac turpis a velit vestibulum lacinia eget ac
+          dui. Nunc efficitur libero tortor, quis aliquam nunc tristique eu. In
+          egestas lectus a porta imperdie
+        </p>
+      </div>
+      {/* <SocialFollow /> */}
     </div>
   );
 }
