8fedccd7a94a270331b0d99cd36fa4780a215fac
Oblivion233 <lucaaas2333@outlook.com>
2020-10-09 20:56:41 +1300

    Updates

---
+++ precache-manifest.8a498a7b358a8e7b2bcb7bb1cedb2f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2c57aaffdd2d6202583ccc288942b1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b7f752c9ae986e17343f",
+    "url": "/react-portfolio/static/css/main.965988d1.chunk.css"
+  },
+  {
+    "revision": "a7b8ba5c489fdd898710",
+    "url": "/react-portfolio/static/js/2.4967c10c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.4967c10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f752c9ae986e17343f",
+    "url": "/react-portfolio/static/js/main.a3b4d95f.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "a3d76de7e5e4fde6589dca97286f9ae9",
+    "url": "/react-portfolio/static/media/face.a3d76de7.jpg"
+  }
+]);
