cdfcd89b6e86e80c8473d357d98f87675d0fe849
Oblivion233 <lucaaas2333@outlook.com>
2021-02-02 19:17:15 +1300

    changed fonts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,19 @@ import { NavBar, Main, About, Resume, Contact, Portfolio } from "./pages";
   /* <meta name="viewport" content="width=device-width, initial-scale=1"></meta> */
 }
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <NavBar />
+//       <Main />
+//       <Portfolio />
+//       <Resume />
+//       <About />
+//       <Contact />
+//     </div>
+//   );
+// }
+
 function App() {
   return (
     <div className="App">
