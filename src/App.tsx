456865149a27125a1c1b32c8c1bd9faab4deaa4b
Oblivion233 <lucaaas2333@outlook.com>
2021-02-10 16:11:18 +1300

    optimized for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -18,20 +18,45 @@ import { NavBar, Main, About, Resume, Contact, Portfolio } from "./pages";
 //   );
 // }
 
-function App() {
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const [height, setHeight] = React.useState(window.innerHeight);
+function Mobile() {
   return (
-    <div className="App">
-      <div className="mobile"></div>
-      <NavBar />
-      <Main />
-      <Portfolio />
-      <Resume />
-      <About />
-      <Contact />
+    <div className="mobile">
+      Website not optimized for small screens <br />
+      please resize window and refresh <br />
+      or view on desktop 😋
     </div>
   );
+}
+
+function App() {
+  const w = window.innerWidth;
+  console.log(w);
+  if (w >= 1040) {
+    return (
+      <div className="App">
+        <NavBar />
+        <Main />
+        <Portfolio />
+        <Resume />
+        <About />
+        <Contact />
+      </div>
+    );
+  } else {
+    return <Mobile />;
+  }
+
+  // return (
+  //   <div className="App">
+  //     <div className="mobile"></div>
+  //     <NavBar />
+  //     <Main />
+  //     <Portfolio />
+  //     <Resume />
+  //     <About />
+  //     <Contact />
+  //   </div>
+  // );
 }
 
 export default App;
