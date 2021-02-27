2551deee6f7afb6c0fecb6bb52b6bc82cc3004b6
Oblivion233 <lucaaas2333@outlook.com>
2021-02-27 22:55:22 +1300

    updated cv

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -68,7 +68,7 @@ const About = () => {
             </Link>
             <a
               style={{ display: "table-cell" }}
-              href="https://drive.google.com/file/d/1t0DrH1Tlajq4EbGw2_90a7Ju92ZY2p2S/view?usp=sharing"
+              href="https://drive.google.com/file/d/1A7LYglM0bFUHmY1TLNogag8a4OP00y3A/view?usp=sharing"
               target="_blank"
               // download
               className={s.cvBtn}
