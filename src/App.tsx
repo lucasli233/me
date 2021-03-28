1bd50db6404ca096e7d0534e16ac549ed833e6c0
Oblivion233 <lucaaas2333@outlook.com>
2021-03-29 05:17:58 +1300

    minor bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,35 @@
-import React from "react";
-import "./App.scss";
-import { NavBar, Main, About, Resume, Contact, Portfolio } from "./pages";
+import React from "react"
+import "./App.scss"
+import { NavBar, Main, About, Resume, Contact, Portfolio } from "./pages"
 
 function Mobile() {
-  return (
-    <div className="mobile">
-      Website not optimized for small screens <br />
-      please resize window and refresh <br />
-      or view on desktop 😋
-    </div>
-  );
+	return (
+		<div className="mobile">
+			Website not optimized for small screens <br />
+			please resize window and refresh <br />
+			or view on desktop 😋
+		</div>
+	)
+}
+
+function Desktop() {
+	return (
+		<div className="App">
+			<NavBar />
+			<Main />
+			<Portfolio />
+			<Resume />
+			<About />
+			<Contact />
+		</div>
+	)
 }
 
 function App() {
-  const w = window.innerWidth;
-  console.log(w);
-  if (w >= 1040) {
-    return (
-      <div className="App">
-        <NavBar />
-        <Main />
-        <Portfolio />
-        <Resume />
-        <About />
-        <Contact />
-      </div>
-    );
-  } else {
-    return <Mobile />;
-  }
-
-  // return (
-  //   <div className="App">
-  //     <div className="mobile"></div>
-  //     <NavBar />
-  //     <Main />
-  //     <Portfolio />
-  //     <Resume />
-  //     <About />
-  //     <Contact />
-  //   </div>
-  // );
+	const w = window.innerWidth
+	// console.log(w);
+	if (w >= 1040) return <Desktop />
+	else return <Mobile />
 }
 
-export default App;
+export default App
